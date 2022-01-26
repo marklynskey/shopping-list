d84f0f1a8a54984938cccc7aa43a5699547d9619
Mark Lynskey <mlynskey2@gmail.com>
2022-01-26 22:13:38 +1300

    build: add eslint-plugin-sort-imports-es6-autofix

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";
-import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
