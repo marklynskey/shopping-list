70210c1c17e3fc7b836265b56cb8462cb81ec08e
Mark Lynskey <mlynskey2@gmail.com>
2022-09-10 22:51:48 +1200

    add keys to list

--- src/components/BudgetBox.jsx
+++ src/components/BudgetBox.jsx
@@ -1,27 +1,29 @@
 import { Flex, Select, Text } from "@chakra-ui/react";
 
-const BudgetBox = () => (
-  <Flex
-    justifyContent="space-between"
-    alignItems="center"
-    margin={6}
-    padding={6}
-    border="1px"
-    borderColor="gray.200"
-    borderRadius="lg"
-  >
-    <Text as="span">Weekly budget:</Text>
-    <Select w={40}>{generateOptions()}</Select>
-  </Flex>
-);
+const BudgetBox = () => {
+  const generateOptions = () => {
+    const options = [];
+    for (let step = 20; step <= 200; step += 10) {
+      options.push(<option key={step}>{step}</option>);
+    }
 
-const generateOptions = () => {
-  const options = [];
-  for (let step = 20; step <= 200; step += 10) {
-    options.push(<option>{step}</option>);
-  }
+    return options;
+  };
 
-  return options;
+  return (
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      margin={6}
+      padding={6}
+      border="1px"
+      borderColor="gray.200"
+      borderRadius="lg"
+    >
+      <Text as="span">Weekly budget:</Text>
+      <Select w={40}>{generateOptions()}</Select>
+    </Flex>
+  );
 };
 
 export default BudgetBox;
